488316aee711f3c6a455b1c4def277e7b13c2b3e
Julio Cesar <jcmcs177@gmail.com>
2021-02-16 17:37:50 -0300

    Changing C code a little bit

--- test/test_c.c
+++ test/test_c.c
@@ -7,13 +7,16 @@ gcc test_c.c ../Log.h
 
 int main()
 {
+    // Initiate log
     if (!LogInit())
         return 1;
 
+    // Write line with \n at the end
     LogWriteLine("testing c log line");
+
+    // Write line with no \n at the end
     LogWrite("testing c log");
 
     LogFinish();
-
     return 0;
 }
