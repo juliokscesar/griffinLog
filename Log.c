03f36cad66421b3aa715136be173ecd80a4f633e
Julio Cesar <jcmcs177@gmail.com>
2021-02-22 11:26:41 -0300

    Back to allocating memory to new log on logwritef to prevent future problems

--- Log.c
+++ Log.c
@@ -48,7 +48,7 @@ void LogWrite(const char* logInfo)
 {
     char* dateTime = GetCurrentDateTime();
 
-    fprintf(logFile, "%s %s", dateTime, logInfo);
+    fprintf(logFile, "[%s] %s", dateTime, logInfo);
 
     free(dateTime);
 }
@@ -57,7 +57,7 @@ void LogWriteLine(const char* logInfo)
 {
     char* dateTime = GetCurrentDateTime();
 
-    fprintf(logFile, "%s %s\n", dateTime, logInfo);
+    fprintf(logFile, "[%s] %s\n", dateTime, logInfo);
 
     free(dateTime);
 }
@@ -67,17 +67,14 @@ void LogWriteF(const char* formatLog, ...)
     va_list args;
     va_start(args, formatLog);
 
-    // const int logSize = (int)strlen(formatLog) + 256;
-    // char* log = malloc(logSize);
+    const int logSize = (int)strlen(formatLog) + 256;
+    char* log = malloc(logSize);
 
-    // vsprintf(log, formatLog, args);
+    vsprintf(log, formatLog, args);
 
-    // LogWrite(log);
+    LogWrite(log);
 
-    // free(log);
-
-    fprintf(logFile, GetCurrentDateTime());
-    vfprintf(logFile, formatLog, args);
+    free(log);
 
     va_end(args);
 }
@@ -99,7 +96,7 @@ char* GetCurrentDateTime()
 
     char* timeNow = malloc(sizeof(char) * 21);
 
-    sprintf(timeNow, "[%d-%d-%d;%dh%dm%ds]", now->tm_year + 1900, now->tm_mon + 1, now->tm_mday, now->tm_hour, now->tm_min, now->tm_sec);
+    sprintf(timeNow, "%d-%d-%d;%dh%dm%ds", now->tm_year + 1900, now->tm_mon + 1, now->tm_mday, now->tm_hour, now->tm_min, now->tm_sec);
 
     return timeNow;
 }
